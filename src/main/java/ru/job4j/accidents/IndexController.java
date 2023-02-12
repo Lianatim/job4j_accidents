4adabb98ffcf74b59f5856a076918dc483f5388c
Liana <95958723+Lianatim@users.noreply.github.com>
2023-02-12 17:29:51 +0300

    1. Загрузка данных в вид

--- src/main/java/ru/job4j/accidents/IndexController.java
+++ src/main/java/ru/job4j/accidents/IndexController.java
@@ -1,13 +1,15 @@
 package ru.job4j.accidents;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping
-    public String showHomePage() {
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
